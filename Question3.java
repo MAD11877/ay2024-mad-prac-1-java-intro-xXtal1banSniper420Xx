befbd76b8a832947d7734a57f0ef0940c621906b
xXtal1banSniper420Xx <167509287+xXtal1banSniper420Xx@users.noreply.github.com>
2024-04-19 15:58:12 +0800

    .

--- Question3.java
+++ Question3.java
@@ -4,7 +4,7 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    System.out.print("Please enter an integer: ");
+    //System.out.print("Please enter an integer: ");
 
     Scanner in = new Scanner(System.in);
 
@@ -12,7 +12,7 @@ public class Question3
 
     int result = number * number;
 
-    System.out.println("The result of multiplying the number by itself is: " + result);
+    System.out.println(result);
 
     in.close();
   }
