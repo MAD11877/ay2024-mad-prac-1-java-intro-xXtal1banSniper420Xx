658a34a8f13520d9e1507df6035c49034302dfa8
xXtal1banSniper420Xx <167509287+xXtal1banSniper420Xx@users.noreply.github.com>
2024-04-19 15:38:59 +0800

    .

--- Question3.java
+++ Question3.java
@@ -4,13 +4,16 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
+    //System.out.print("Please enter an integer: ");
+
     Scanner in = new Scanner(System.in);
-    
+
+    int number = in.nextInt();
+
+    int result = number * number;
+
+    //System.out.println("The result of multiplying the number by itself is: " + result);
+
+    in.close();
   }
 }
