3dcfde0cb782fa18b5b0c4cd1e8fee1813a51d8e
xXtal1banSniper420Xx <167509287+xXtal1banSniper420Xx@users.noreply.github.com>
2024-04-19 15:59:45 +0800

    .

--- Question4.java
+++ Question4.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
-    Scanner in = new Scanner(System.in);
-    
-  }
+public class Question4 {
+    public static void main(String[] args) {
+        //System.out.print("Enter the base size of the triangle: ");
+
+        Scanner in = new Scanner(System.in);
+
+        int baseSize = in.nextInt();
+
+        for (int i = baseSize; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        in.close();
+    }
 }
+
